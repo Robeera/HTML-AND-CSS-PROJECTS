d891367c76e72e8f3b65a8a4610e39bda2fedb83
Robeera Bedassa <bedassabrenden11@gmail.com>
2025-11-19 22:44:53 -0800

    Basic HTML and JavaScript files

---
+++ BASIC_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,9 @@
+
+alert("Text");
+
+function My_First_Function() {
+						var string = "Kiss me, I'm Irish!";
+						var result = String.fontcolor("green");
+						document.getElementById("Irish").innerHMTL = 
+						result;
+						}
